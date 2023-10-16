7196da31962bf817a2fa8b007f42d70607c2c251
alexdzehil <a.dzehil@gmail.com>
2023-10-16 21:40:39 +0300

    Add weather API and corresponding route
    This commit introduces an API endpoint to fetch weather information for any given city. The new endpoint '/weather/' allows to pass a city name as a parameter and fetches the respective weather details including temperature, pressure, and wind speed. The Django Ninja framework has been added to requirements for building APIs quickly with type hints and easy debugging features. Additionally, url for the new API has been added in app/urls.py file. This feature improves usability by providing a simple way to access weather data.

--- apps/api/views.py
+++ apps/api/views.py
@@ -1,3 +1,22 @@
-from django.shortcuts import render
+from ninja import NinjaAPI
 
-# Create your views here.
+from apps.weather.gps_api_service import get_coordinates
+from apps.weather.weather_api_service import get_weather
+
+api = NinjaAPI()
+
+
+@api.get('/weather/')
+def get_weather_by_city_name(request, city: str):
+    if city:
+        coordinates = get_coordinates(city.lower())
+        if coordinates:
+            weather = get_weather(coordinates)
+            if weather:
+                data = {
+                    'temperature': weather.temperature,
+                    'pressure': weather.pressure,
+                    'wind_speed': weather.wind_speed,
+                }
+                return data
+        return {'error': 'something went wrong'}
